74196fd7a147090e0c7c0521d257dfec7c372368
vinitavictushvi <vinitabind1923@gmail.com>
2026-05-26 12:38:51 +0530

    fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/pages//Home";
+
+import Home from "./components/pages/Home";
 import Portfolio from "./components/pages/Portfolio";
 import Services from "./components/pages/Services";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
-
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/services" element={<Services/>} />
-        <Route path="/About" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
